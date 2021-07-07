6e5116a46ee732c6beace5fc47b0cff4d01a8d56
Pavel Andreev <Andreev.PK@phystech.edu>
2021-07-07 18:22:47 +0300

    Added script thta changes files in torch

---
+++ change.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+torch_path = "/Vol0/user/p.andreev/anaconda3/envs/pynight/lib/python3.8/site-packages/"
+
+l=  ["torch/quantization/quantization_mappings.py",
+     "torch/nn/qat/modules/conv.py",
+     "torch/nn/modules/conv.py",
+     "torch/nn/intrinsic/modules/fused.py",
+     "torch/nn/intrinsic/quantized/modules/conv_relu.py",
+     "torch/nn/intrinsic/qat/modules/conv_fused.py",
+     "torch/nn/quantized/modules/conv.py",
+     "torch/nn/quantized/modules/functional_modules.py",
+     "torch/nn/qat/modules/__init__.py",
+     "torch/nn/intrinsic/modules/__init__.py",
+     "torch/nn/intrinsic/quantized/modules/__init__.py",
+     "torch/nn/intrinsic/qat/modules/__init__.py"]
+
+for f in l:
+    dest = os.path.join(torch_path, f)
+    shutil.copy("./" + f, dest)
+
+    
